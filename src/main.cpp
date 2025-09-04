b2158d62af4245cd1f2e566759d8e9cba2646ee4
rubenaryo <rubeny484@gmail.com>
2025-09-03 21:29:01 -0400

    Revert "Partial progress on 2.3"
    This reverts commit 1ad947e7ad6a577bbeb16753eafd51f03c94e452.

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@
 // LOOK-2.1 LOOK-2.3 - toggles for UNIFORM_GRID and COHERENT_GRID
 #define VISUALIZE 1
 #define UNIFORM_GRID 1
-#define COHERENT_GRID 1
+#define COHERENT_GRID 0
 
 // LOOK-1.2 - change this to adjust particle count in the simulation
 const int N_FOR_VIS = 5000;
@@ -34,7 +34,7 @@ const float DT = 0.2f;
 * C main function.
 */
 int main(int argc, char* argv[]) {
-  projectName = "Ruben Young";
+  projectName = "5650 CUDA Intro: Boids";
 
   if (init(argc, argv)) {
     mainLoop();
