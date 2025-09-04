f57db6d710eed4166508f07e1ee41ea4a5f4a733
Jacky Park <jackypark9852@gmail.com>
2025-09-04 16:12:41 -0400

    feat: implement rule 1

--- src/kernel.cu
+++ src/kernel.cu
@@ -241,9 +241,35 @@ void Boids::copyBoidsToVBO(float *vbodptr_positions, float *vbodptr_velocities) 
 */
 __device__ glm::vec3 computeVelocityChange(int N, int iSelf, const glm::vec3 *pos, const glm::vec3 *vel) {
   // Rule 1: boids fly towards their local perceived center of mass, which excludes themselves
+    glm::vec3 posSelf = pos[iSelf]; 
+    glm::vec3 posOther; 
+    glm::vec3 pCenter; 
+    glm::vec3 v1; 
+    unsigned count = 0; 
+    for (int i = 0; i < N; ++i) {
+        if (i == iSelf) {
+            continue; 
+        }
+        
+        posOther = pos[i]; 
+        float dist = glm::distance(posSelf, pos[i]); 
+        if (dist < rule1Distance) {
+            pCenter += posOther;
+            ++count;
+        }
+
+    }
+    
+    if (count != 0) {
+        pCenter = pCenter / glm::vec3(count);
+        v1 = (pCenter - posSelf) * rule1Scale;
+    }
+
   // Rule 2: boids try to stay a distance d away from each other
+    glm::vec3 v2; 
   // Rule 3: boids try to match the speed of surrounding boids
-  return glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 v3; 
+  return v1;
 }
 
 /**
@@ -252,9 +278,20 @@ __device__ glm::vec3 computeVelocityChange(int N, int iSelf, const glm::vec3 *po
 */
 __global__ void kernUpdateVelocityBruteForce(int N, glm::vec3 *pos,
   glm::vec3 *vel1, glm::vec3 *vel2) {
+    int index = threadIdx.x + (blockIdx.x * blockDim.x);
+    if (index >= N) {
+        return;
+    }
+
   // Compute a new velocity based on pos and vel1
+    glm::vec3 speedDelt = computeVelocityChange(N, index, pos, vel1);
+    glm::vec3 newSpeed = vel1[index] + speedDelt;
+
   // Clamp the speed
+    newSpeed = glm::normalize(newSpeed) * maxSpeed; 
+
   // Record the new velocity into vel2. Question: why NOT vel1?
+    vel2[index] = newSpeed; 
 }
 
 /**
@@ -357,8 +394,14 @@ __global__ void kernUpdateVelNeighborSearchCoherent(
 * Step the entire N-body simulation by `dt` seconds.
 */
 void Boids::stepSimulationNaive(float dt) {
+
   // TODO-1.2 - use the kernels you wrote to step the simulation forward in time.
+    dim3 fullBlocksPerGrid((numObjects + blockSize - 1) / blockSize);
+    kernUpdateVelocityBruteForce << <fullBlocksPerGrid, blockSize >> > (numObjects, dev_pos, dev_vel1, dev_vel2); 
+    kernUpdatePos << < fullBlocksPerGrid, blockSize >> > (numObjects, dt, dev_pos, dev_vel2);
+
   // TODO-1.2 ping-pong the velocity buffers
+    std::swap(dev_vel1, dev_vel2); 
 }
 
 void Boids::stepSimulationScatteredGrid(float dt) {
